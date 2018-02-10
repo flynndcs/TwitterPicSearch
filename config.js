4e634a65e138a1524497fa6a5edf7fec6a343ed3
Daniel Flynn <21161368+flynndcs@users.noreply.github.com>
2018-02-10 17:28:36 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         'fCDldPx5OYNKxZjBMi6WhB1py',
-  consumer_secret:      '3vwOPJm3YW9JnXTjTKWna9xt44Inl7dopbT1uN4ovZPjyegRrp',
-  access_token:         '708408234994900992-1CkrWoEIlFjkU2oxaEUFuaJdF3E2nwF',
-  access_token_secret:  'vsxw3FrWjxKHV2mG20ZMQ0UG8qitFPuRrLdxXotKOCwLn',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all
-}
